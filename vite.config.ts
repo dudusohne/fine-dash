6ca81bb0c479df2e1617c92af6e5a218e78a4944
dudusohne <dudu.hit@gmail.com>
2023-05-12 11:18:25 -0300

    feat: new configs

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react(),  VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    react(), 
+    VitePWA({ registerType: 'autoUpdate' })
+  ],
 });
