2b0f8af47514749d350b09ae3ca0ff30194a6a73
dudusohne <dudu.hit@gmail.com>
2023-05-30 11:49:46 -0300

    feat: added remote app cfg

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import federation from '@originjs/vite-plugin-federation';
 
 export default defineConfig({
   plugins: [
-    react(), 
-    VitePWA({ registerType: 'autoUpdate' })
+    react(),
+    VitePWA({ registerType: 'autoUpdate' }),
+    federation({
+      name: 'remote-app',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './App': './src/App.tsx',
+      },
+      remotes: {
+        foo: 'remote_foo',
+      },
+      shared: ['react', 'react-dom'],
+    }),
   ],
 });
