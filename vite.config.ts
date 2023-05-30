dad0009890bfe0acd033cc73d1b1090c8757ef2b
dudusohne <dudu.hit@gmail.com>
2023-05-30 11:40:40 -0300

    feat: added remote federation config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import federation from '@originjs/vite-plugin-federation';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: 'remote-app',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './App': './src/App.tsx',
+      },
+      remotes: {
+        foo: 'remote_foo',
+      },
+      shared: ['react', 'react-dom', 'react-router-dom'],
+    }),
+  ],
 });
