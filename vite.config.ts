d19cb226f20de71464be2def4fe4f0d03bce571c
dudusohne <dudu.hit@gmail.com>
2023-06-06 16:44:46 -0300

    fix: remove unused mfe imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
-import federation from '@originjs/vite-plugin-federation';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({ registerType: 'autoUpdate' }),
-    federation({
-      name: 'remote-app',
-      filename: 'remoteEntry.js',
-      exposes: {
-        './App': './src/App.tsx',
-      },
-      remotes: {
-        foo: 'remote_foo',
-      },
-      shared: ['react', 'react-dom'],
-    }),
-  ],
+  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
 });
